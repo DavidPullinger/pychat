0d0dad4efb5745e980856d417300326496dedfc0
David Pullinger <davpullinger@gmail.com>
2022-03-09 09:17:39 +0200

    Add Nivan's files - UI skeleton and basic client functionality

---
+++ chatUI.py
@@ -0,0 +1,114 @@
+from tkinter import *
+from tkinter import messagebox
+import json
+import client
+
+
+root = Tk()
+root.title("ChatApp")
+root.geometry("800x600")
+global chats    #list of chats
+#MAIN SCREEN######################################
+def funclogin():
+    action,body = client.reqLOGIN(uname.get(),psword.get())
+    client.send(action,body)    #sends message to server
+    body = client.receive()
+    if(body == "False"):
+        messagebox.showerror("showerror", "Incorrect login details") 
+    else:
+        body = json.loads(body)
+        for i in body:
+            #add chats to global chats list
+            chats.append()
+        chatscreen()
+        
+
+def funccreateacc():
+    action,body = client.reqCREATE_ACC(uname.get(),psword.get())
+    client.send(action,body)    #send message to server
+    if(client.receive()):
+        chatscreen()
+        messagebox.showinfo("showinfo", "Account created!")
+    else:
+        messagebox.showerror("showerror", "USERNAME ALREADY EXISTS")    
+    return 0
+
+title = Label(root,text = "ChatApp", font = ("Calibri",30) ).grid(row = 0, column = 1)
+lbluname = Label(root, text = "username:").grid(row = 1, column = 0)
+lblpsword = Label(root, text = "password:").grid(row = 2, column = 0)
+uname = StringVar()
+psword = StringVar()
+etryuname = Entry(root, textvariable = uname).grid(row = 1, column = 1)
+etrypsword = Entry(root,textvariable=psword).grid(row = 2,column = 1)
+
+btnlogin = Button(root,text = "Login", command = funclogin).grid(row = 3, column = 1)
+btncreateacc = Button(root, text = "Create Account", command = funccreateacc).grid(row = 4, column = 1)
+
+
+#CHAT choose SCREEN######################################
+#send UPDATE_MSGS regularly
+def chatscreen():
+    chatscr = Toplevel()
+    chatscr.geometry("800x600")
+    chatscr.title("CHATSCREEN")
+    btncreategrp = Button(chatscr,text = "Create New group" , command = newchatscreen).pack()
+    chat = StringVar()
+    chats = ["chat1", "chat2", "chat3"]
+    drpchats = OptionMenu(chatscr,chat, *chats, command =lambda chat: openchat(chat)).pack()
+    
+    
+#NEW CHAT SCREEN##################################################
+def funccreatechat():
+    action,body = client.reqCREATE_GROUP(grpname.get(),participants.get().split(", "))
+    client.send(action,body)    #send message to server
+    bod = client.receive()
+    if(bod == "False"):
+        messagebox.showerror("showerror", "PARTICIPANTS NOT VALID")
+        
+        
+    else:
+        #add group to list bod containts group ID
+        messagebox.showinfo("showinfo", "Group Created!")
+        
+        
+        
+
+
+def newchatscreen():
+    newchatscr = Toplevel()
+    newchatscr.title("CREAT NEW CHAT")
+    newchatscr.geometry("800x600")
+    global grpname
+    global participants
+    grpname = StringVar()
+    participants = StringVar()
+    lblname = Label(newchatscr,text = "Enter name of group").grid(row = 0, column = 0)
+    etryname = Entry(newchatscr, textvariable=grpname).grid(row = 0, column = 1)
+    lblparticipants = Label(newchatscr, text = "Enter a list of usernames\nSeparated by a ,").grid(row = 1, column = 0)
+    etryparticipants = Entry(newchatscr, textvariable=participants).grid(row = 1, column = 1)
+    btncreatechat = Button(newchatscr,text = "Create Chat", command =funccreatechat).grid(row = 2, column = 0)
+    
+#CHAT SCREEN#############################################
+def sendmsg(msg):
+    print(msg)
+
+def openchat(chatname):
+    openchatscr = Toplevel()
+    openchatscr.geometry("800x600")
+    openchatscr.title("opened chat")
+    print(chatname)
+    mainframe = LabelFrame(openchatscr)
+    mainframe.grid(row = 0,column = 0, columnspan=3)
+    chatbox = Text(mainframe, width = 50, height = 30)
+    chatbox.insert('1.0', "First message in chat")
+    #chatbox.grid(row = 0, colum = 0, columnspan=3)
+    chatbox.pack(expand = 1,fill = BOTH)
+    etrymsg = Entry(openchatscr).grid(row = 1, column = 0, columnspan=2)
+    btnsend = Button(openchatscr,text = "SEND",command =lambda msg: sendmsg(msg)).grid(row = 1, column = 1, columnspan=2)
+########################################
+#while True:
+#    root.update()
+#    loop()
+    
+
+root.mainloop()
